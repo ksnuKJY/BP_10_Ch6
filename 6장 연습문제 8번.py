4fc363dae8ce9e1904b41c33269482f5f10ec30d
jinyong <112368808+ksnuKJY@users.noreply.github.com>
2022-11-12 21:14:59 +0900

    Add files via upload

---
+++ 6장 연습문제 8번.py
@@ -0,0 +1,11 @@
+import turtle
+
+myPen=turtle.Turtle()
+myPen.speed(0)
+myPen.color("#FF0000")
+
+for j in range (1,10):
+  for i in range (1,6):
+    myPen.lt(144)
+    myPen.fd(200)
+  myPen.lt(10)
